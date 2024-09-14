c20604394deda4f069a67584b8644e2564a1f6b6
Duy Tai <0985203883tai@gmail.com>
2024-09-14 22:20:24 +0700

    first commit

---
+++ app/src/main/java/com/duytai/btxulysukien/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.duytai.btxulysukien;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button btn_open_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        btn_open_main = findViewById(R.id.btn_open_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_open_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent myIntent = new Intent(SecondActivity.this, MainActivity.class);
+//                startActivity(myIntent);
+                finish();
+            }
+        });
+    }
+}
